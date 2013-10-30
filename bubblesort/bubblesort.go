456618a58a14459003fa1445cc9672dbc423a80a
Anton Lindström <lindztr@gmail.com>
2013-10-30 18:29:52 +0100

    add bubblesort

---
+++ bubblesort/bubblesort.go
@@ -0,0 +1,23 @@
+package bubblesort
+
+func Sort(set []int) []int {
+	swapped := false
+
+	for i := 1; i < len(set); i++ {
+		if set[i-1] > set[i] {
+			a, b := set[i-1], set[i]
+
+			set[i-1] = b
+			set[i] = a
+			// swap
+			swapped = true
+		}
+	}
+
+	// recurse until we don't swap
+	if swapped {
+		set = Sort(set)
+	}
+
+	return set
+}

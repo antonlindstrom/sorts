00319922b22bef5b58e4212ef69f7b4bc7f3e154
Anton Lindström <lindztr@gmail.com>
2013-12-09 18:13:25 +0100

    add combsort

---
+++ combsort/combsort.go
@@ -0,0 +1,28 @@
+package combsort
+
+func Sort(set []int) []int {
+	gap := len(set)
+	shrink := 1.3
+	swapped := false
+
+	for gap > 1 || swapped {
+		gap = int(float64(gap) / shrink)
+
+		if gap < 1 {
+			gap = 1
+		}
+
+		swapped = false
+
+		for i := 0; gap+i < len(set); i++ {
+			if set[i]-set[i+gap] > 0 {
+				swap := set[i]
+				set[i] = set[i+gap]
+				set[i+gap] = swap
+				swapped = true
+			}
+		}
+	}
+
+	return set
+}
---
+++ combsort/combsort_test.go
@@ -0,0 +1,25 @@
+package combsort
+
+import "testing"
+
+func TestSort(t *testing.T) {
+	sortedSet := Sort([]int{1})
+
+	if sortedSet[0] != 1 {
+		t.Fatal("Could not sort dataset with set 1!")
+	}
+
+	sortedSet = Sort([]int{10, 9, 4, 7, 3, 1, 8, 5, 2, 6})
+
+	for i := 0; i < 10; i++ {
+		if sortedSet[i] != i+1 {
+			t.Fatal("Could not sort dataset!")
+		}
+	}
+}
+
+func BenchmarkSort(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sort([]int{10, 9, 4, 7, 3, 1, 8, 5, 2, 6})
+	}
+}

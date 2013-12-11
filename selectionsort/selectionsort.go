014ed899ac8892c7b1fbd33c136adcbae5df984e
Anton Lindström <lindztr@gmail.com>
2013-12-11 16:32:56 +0000

    add selectionsort

---
+++ selectionsort/selectionsort.go
@@ -0,0 +1,21 @@
+package selectionsort
+
+func Sort(set []int) []int {
+	min := 0
+
+	for j := 0; j < len(set)-1; j++ {
+		min = j
+
+		for i := j + 1; i < len(set); i++ {
+			if set[i] < set[min] {
+				min = i
+			}
+		}
+
+		if min != j {
+			set[j], set[min] = set[min], set[j]
+		}
+	}
+
+	return set
+}
